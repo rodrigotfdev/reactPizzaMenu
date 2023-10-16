7f9f385163becfba7e24c899896353e377fec506
Rodrigo <rtf.franca@gmail.com>
2023-10-16 11:45:36 -0300

    Rendering list of pizzas using Map method

--- src/components/Pizza.jsx
+++ src/components/Pizza.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import Menu from './Menu'
 
-export default function Pizza() {
+export default function Pizza({pizzaObj}) {
   return (
-    <div>Pizza</div>
+    <li className='pizza'>
+        <img src={pizzaObj.photoName} alt="" />
+        <div>
+            <h3>{pizzaObj.name}</h3>
+            <p>{pizzaObj.ingredients}</p>
+            <span>{pizzaObj.price}</span>
+        </div>
+    </li>
   )
 }
