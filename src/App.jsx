0045601b7621224434f14021161468e56884e112
Rodrigo <rtf.franca@gmail.com>
2023-10-16 11:39:49 -0300

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './components/Header'
+import Menu from './components/Menu'
+import Footer from './components/Footer'
+
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  )
+}
---
+++ src/components/Menu.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Menu() {
+  return (
+    <div className='menu'>
+        <h2>Our menu</h2>
+
+        
+    </div>
+  )
+}
